b83c5098005aa649d27a4fd3d842c45901a0be51
Arya <alinikh1380@gmail.com>
2023-08-20 09:57:22 +0330

    improved a little bit

--- computerGuessTheNumber.py
+++ computerGuessTheNumber.py
@@ -10,3 +10,5 @@ print('|| Type "s", "b", if your number is smaller,bigger or "c" if computer gue
 try_count = 1
 maxi = 1000
 mini = -1000
+computer_guess = random.randint(-1000,1000) #computer first guess can be 0 to find the number faster
+status = input(f'{Fore.CYAN}My first guess is {computer_guess}.{Fore.RESET}{Fore.YELLOW}\nhow is your number (type "c" if my guess was right)? {Fore.RESET}')
